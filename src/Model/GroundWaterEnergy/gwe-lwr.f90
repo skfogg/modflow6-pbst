6702860132d5206a242e390ada452e16413036b2
S. Katie Fogg <s.katie.fogg@gmail.com>
2025-10-28 16:01:57 -0600

    Shf lwr (#25)

--- src/Model/GroundWaterEnergy/gwe-lwr.f90
+++ src/Model/GroundWaterEnergy/gwe-lwr.f90
@@ -1,27 +1,24 @@
 !> @brief This module contains methods for calculating the longwave radiation heat flux
 !!
-!! This module contains the methods used to calculate the longwave radiation
-!! heat flux for surface-water boundaries, like streams and lakes.  In its
-!! current form, this class acts like a package to a package, similar to the
-!! TVK package (or utiliity) that can be invoked from the NPF package.  Once
-!! this package is completed in its prototyped form, it will likely be moved
-!! around.
+!! This module contains the methods used to calculate the longwave radiation heat flux
+!! for surface-water boundaries, like streams and lakes.  In its current form,
+!! this class acts like a package to a package, similar to the TVK package that
+!! can be invoked from the NPF package.  Once this package is completed in its
+!! prototyped form, it will likely be moved around.
 !<
 
 module LongwaveModule
-
-  use ConstantsModule, only: LINELENGTH, LENMEMPATH, DZERO, LENVARNAME, &
-                             DSTEFANBOLTZMANN, DFOUR
+  use ConstantsModule, only: LINELENGTH, LENMEMPATH, DZERO, LENVARNAME, DSTEFANBOLTZMANN, DCTOK
   use KindModule, only: I4B, DP
   use MemoryManagerModule, only: mem_setptr
   use MemoryHelperModule, only: create_mem_path
-  use TimeSeriesLinkModule, only: TimeSeriesLinkType
-  use TimeSeriesManagerModule, only: TimeSeriesManagerType, tsmanager_cr
   use SimModule, only: store_error
   use SimVariablesModule, only: errmsg
-  use PbstBaseModule !, only: PbstBaseType, pbstbase_da
+  use PbstBaseModule, only: PbstBaseType, pbstbase_da
 
   implicit none
+
+  private
 
   public :: LwrType
   public :: lwr_cr
@@ -30,23 +27,21 @@ module LongwaveModule
 
   type, extends(PbstBaseType) :: LwrType
 
-    real(DP), pointer :: lwrefl => null() !< longwave reflectance of water surface
-    real(DP), pointer :: emissw => null() !< emissivity of water
-    real(DP), pointer :: emissr => null() !< emissivity of riparian canopy
-    real(DP), dimension(:), pointer, contiguous :: shd => null() !< shade fraction
-    real(DP), dimension(:), pointer, contiguous :: atmc => null() !< atmospheric composition adjustment
-    real(DP), dimension(:), pointer, contiguous :: tatm => null() !< temperature of the atmosphere
-    real(DP), dimension(:), pointer, contiguous :: rh => null() !< relative humidity
-    real(DP), dimension(:), pointer, contiguous :: ea => null() !< ambient vapor pressure of the atmosphere
-    real(DP), dimension(:), pointer, contiguous :: es => null() !< saturation vapor pressure at air temperature
-    real(DP), dimension(:), pointer, contiguous :: ew => null() !< saturation vapor pressure at water temperature
+    real(DP), pointer :: lwrefl => null() !< ABC longwave reflectance of water surface 
+    real(DP), pointer :: emissw => null() !< ABC emissivity of water
+    real(DP), pointer :: emissr => null() !< ABC emissivity of riparian canopy
+    real(DP), dimension(:), pointer, contiguous :: shd => null() !< ABC shade fraction
+    real(DP), dimension(:), pointer, contiguous :: atmc => null() !< ABC atmospheric composition adjustment
+    real(DP), dimension(:), pointer, contiguous :: tatm => null() !< ABC temperature of the atmosphere
+    real(DP), dimension(:), pointer, contiguous :: rh => null() !< ABC relative humidity
 
   contains
 
     procedure :: da => lwr_da
-    procedure :: pbst_ar => lwr_ar_set_pointers
+    procedure :: read_option => lwr_read_option
+    procedure :: ar_set_pointers => lwr_ar_set_pointers
+    procedure :: get_pointer_to_value => lwr_get_pointer_to_value
     procedure, public :: lwr_cq
-    procedure, private :: calc_lwr
 
   end type LwrType
 
@@ -68,9 +63,10 @@ contains
     allocate (this)
     call this%init(name_model, 'LWR', 'LWR', inunit, iout, ncv)
     this%text = text
+    !
   end subroutine lwr_cr
 
-  !> @brief Announce package and set pointers to variables
+   !> @brief Announce package and set pointers to variables
   !!
   !! Announce package version and set array and variable pointers from the ABC
   !! package for access by LWR.
@@ -80,68 +76,84 @@ contains
     class(LwrType) :: this
     ! -- local
     character(len=LENMEMPATH) :: abcMemoryPath
+    ! -- formats
+    character(len=*), parameter :: fmtlwr = &
+      "(1x,/1x,'LWR -- LONGWAVE RADIATION PACKAGE, VERSION 1, 05/01/2025', &
+      &' INPUT READ FROM UNIT ', i0, //)"
     !
-    ! -- print a message noting that the LWR utility is active
-    write (this%iout, '(a)') &
-      'LWR -- LONGWAVE RADIATION WILL BE INCLUDED IN THE ATMOSPHERIC '// &
-      'BOUNDARY CONDITIONS FOR THE STREAMFLOW ENERGY TRANSPORT PACKAGE'
+    ! -- Print a message identifying the LWR package
+    write (this%iout, fmtlwr) this%inunit
     !
-    ! -- set pointers to variables hosted in the ABC package
+    ! -- Set pointers to other package variables
+    ! -- ABC
     abcMemoryPath = create_mem_path(this%name_model, 'ABC')
     call mem_setptr(this%lwrefl, 'LWREFL', abcMemoryPath)
     call mem_setptr(this%shd, 'SHD', abcMemoryPath)
     call mem_setptr(this%atmc, 'ATMC', abcMemoryPath)
     call mem_setptr(this%tatm, 'TATM', abcMemoryPath)
     call mem_setptr(this%emissw, 'EMISSW', abcMemoryPath)
-    call mem_setptr(this%emissr, 'EMISSR', abcMemoryPath)
+    call mem_setptr(this%emissw, 'EMISSR', abcMemoryPath)
     call mem_setptr(this%rh, 'RH', abcMemoryPath)
-    call mem_setptr(this%ea, 'EA', abcMemoryPath)
-    call mem_setptr(this%es, 'ES', abcMemoryPath)
-    call mem_setptr(this%ew, 'EW', abcMemoryPath)
-    !
-    ! -- set the riparian canopy emissivity constant
-    this%emissr = 0.97_DP
-    !
-    ! -- create time series manager
-    call tsmanager_cr(this%tsmanager, this%iout, &
-                      removeTsLinksOnCompletion=.true., &
-                      extendTsToEndOfSimulation=.true.)
+    
   end subroutine lwr_ar_set_pointers
-
+  
+  !!> @brief Get an array value pointer given a variable name and node index
+  !!!
+  !!! Return a pointer to the given node's value in the appropriate ABC array
+  !!! based on the given variable name string.
+  !!<
+  function lwr_get_pointer_to_value(this, n, varName) result(bndElem)
+      ! -- dummy
+      class(LwrType) :: this
+      integer(I4B), intent(in) :: n
+      character(len=*), intent(in) :: varName
+      ! -- return
+      real(DP), pointer :: bndElem
+      
+      select case(varName)
+      case ('TATM')
+        bndElem => this%tatm(n)
+      case ('ATMC')
+        bndElem => this%atmc(n)
+      case ('RH')
+        bndElem => this%rh(n)  
+      case default
+        bndElem => null()
+      end select
+    end function
+  
   !> @brief Calculate Longwave Radiation Heat Flux
   !!
   !! Calculate and return the longwave radiation heat flux for one reach
   !<
-  subroutine lwr_cq(this, ifno, tstrm, tfac, toff, lwrflx)
+  subroutine lwr_cq(this, ifno, tstrm, lwrflx)
     ! -- dummy
     class(LwrType), intent(inout) :: this
     integer(I4B), intent(in) :: ifno !< stream reach integer id
     real(DP), intent(in) :: tstrm !< temperature of the stream reach
-    real(DP), intent(in) :: tfac !< temperature units adjustment factor 
-    real(DP), intent(in) :: toff !< temperature units offset
     real(DP), intent(inout) :: lwrflx !< calculated longwave radiation heat flux amount
     ! -- local
+    real(DP) :: sat_vap_ta
+    real(DP) :: amb_vap_atm
     real(DP) :: emissa
     real(DP) :: emisss
+    real(DP) :: emissr
     real(DP) :: lwratm
     real(DP) :: lwrstrm
+    
     !
     ! -- intermediate calculations
+    sat_vap_ta = 6.1275_DP*exp(17.2693882_DP*((this%tatm(ifno))/(this%tatm(ifno) + DCTOK - 35.86_DP)))
+    amb_vap_atm = (this%rh(ifno)/100.0_DP)*sat_vap_ta
+    emissa = 1.24_DP*this%atmc(ifno)*((amb_vap_atm/this%tatm(ifno))**(1/7))
     !
-    ! -- atmospheric emissivity (A.14)
-    emissa = this%epsa(this%ea(ifno), tfac * this%tatm(ifno) + toff, this%atmc(ifno))
+    emisss = (1-this%shd(ifno))*emissa + this%shd(ifno)*emissr
     !
-    ! -- shade-altered above-channel emissivity [Eq. 3, Fogg et al. (2023)]
-    emisss = this%epss(this%shd(ifno), emissa, this%emissr)
+    lwratm = emisss*DSTEFANBOLTZMANN*((this%tatm(ifno))**4)
+    lwrstrm = -this%emissw*DSTEFANBOLTZMANN*(tstrm**4)
     !
-    ! -- long wave radiation transmitted from the atmosphere to the water surface (A.12)
-    lwratm = this%calc_lwr(emisss, tfac * this%tatm(ifno) + toff)
-    !
-    ! -- long wave radiation transmitted from water surface to the atmosphere (A.13)
-    lwrstrm = this%calc_lwr(this%emissw, tfac * tstrm + toff)
-    !
-    ! -- longwave radiation heat flux
-    lwrflx = lwratm * (1 - this%lwrefl) - lwrstrm
+    ! -- calculate longwave radiation heat flux
+    lwrflx = lwratm*(1-this%lwrefl) + lwrstrm
   end subroutine lwr_cq
 
   !> @brief Deallocate package memory
@@ -149,10 +161,17 @@ contains
   !! Deallocate TVK package scalars and arrays.
   !<
   subroutine lwr_da(this)
+    ! -- modules
+    !use MemoryManagerModule, only: mem_deallocate
     ! -- dummy
     class(LwrType) :: this
     !
-    ! -- deallocate time series
+    ! -- Nullify pointers to other package variables
+    !call mem_deallocate(this%rhoa)
+    !call mem_deallocate(this%cpa)
+    !call mem_deallocate(this%cd)
+    !
+    ! -- Deallocate time series
     nullify (this%shd)
     nullify (this%lwrefl)
     nullify (this%tatm)
@@ -160,28 +179,25 @@ contains
     nullify (this%emissr)
     nullify (this%atmc)
     nullify (this%rh)
-    nullify (this%ea)
-    nullify (this%es)
-    nullify (this%ew)
     !
-    ! -- deallocate parent
+    ! -- Deallocate parent
     call pbstbase_da(this)
   end subroutine lwr_da
 
-  !> @brief Calculate longwave radiation
+  !> @brief Read a LWR-specific option from the OPTIONS block
   !!
-  !! Function for calculating incoming or outgoing longwave radiation
+  !! Process a single LWR-specific option. Used when reading the OPTIONS block
+  !! of the LWR package input file.
   !<
-  function calc_lwr(this, eps, temp) result(lwr)
+  function lwr_read_option(this, keyword) result(success)
     ! -- dummy
     class(LwrType) :: this
-    real(DP) :: eps !< epsilon, representing either emissivity of the atmosphere, the shade-weighted emissivity of the atm, or emissivity of water
-    real(DP) :: temp !< will be temperature in Kelvin, representing either the temperature of the stream or the atmosphere in degree C
+    character(len=*), intent(in) :: keyword
     ! -- return
-    real(DP) :: lwr !< longwave radiation
+    logical :: success
     !
-    ! -- generalized equation for calculating longwave radiation
-    lwr = eps * DSTEFANBOLTZMANN * ((temp)**DFOUR)
-  end function calc_lwr
+    ! -- There are no LWR-specific options, so just return false
+    success = .false.
+  end function lwr_read_option
 
 end module LongwaveModule
